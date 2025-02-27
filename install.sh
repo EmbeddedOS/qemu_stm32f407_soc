009f59cd2e52894bbfb6b2d6264a3d9268528cbc
Cong Nguyen <72590623+EmbeddedOS@users.noreply.github.com>
2025-02-27 10:21:56 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,7 +2,7 @@
 
 sudo apt-get install python3 python3-pip git libglib2.0-dev libfdt-dev \
     libpixman-1-dev zlib1g-dev ninja-build git-email libaio-dev \
-    libbluetooth-devlibcapstone-dev libbrlapi-dev libbz2-dev libcap-ng-dev \
+    libbluetooth-dev libcapstone-dev libbrlapi-dev libbz2-dev libcap-ng-dev \
     libcurl4-gnutls-dev libgtk-3-dev libibverbs-dev libjpeg8-dev \
     libncurses5-dev libnuma-dev librbd-dev librdmacm-dev libsasl2-dev \
     libsdl2-dev libseccomp-dev libsnappy-dev libssh-dev libvde-dev \
